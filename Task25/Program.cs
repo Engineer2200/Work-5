2afabcfbc2e6ff21997bb06c224ccb80f34ddbd3
Engineer <arsenya.suvorov@mail.ru>
2022-11-28 01:53:53 +0400

    start

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i =1; i <= B; i++)
+    {
+        result = result * A;
+    }
+    Console.WriteLine(result);
