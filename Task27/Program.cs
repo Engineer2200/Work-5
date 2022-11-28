94b89e6e7799f7fa2dd0881219e00fd7af9438b4
Engineer <arsenya.suvorov@mail.ru>
2022-11-28 11:17:31 +0400

    start

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,27 +1,9 @@
-﻿// // int countt(int number)
-// //     {
-// //     int count = 0;   
-// //     if (number != 0)
-// //         {
-// //         while (number > 0)
-// //             { 
-// //               count++;
-// //               number = number/10;  
-// //             } 
-// //         }
-// //         else {count = 1;}
-// //         return count;
-// //     }
-
-// Console.Write("Введите число: ");
-// int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
-// int sum = 0;
-// // int count = countt(number);
-// for (int i = 0; i < arr.Length; i++)
-//     {
-//         sum = sum + number.ToString()[i];
-//     }
-//     Console.WriteLine(sum);
-int[] a = textBox1.Text.Split(' ').
-          Where(x => !string.IsNullOrWhiteSpace( x )).
-          Select(x => int.Parse(x)).ToArray();
+﻿Console.Write("Введите число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int s =0;
+while (number>0)
+{
+s=s+ (number % 10);
+number=number/10;
+}
+Console.WriteLine(s);
